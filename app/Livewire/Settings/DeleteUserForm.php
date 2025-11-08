e61626b95cf9a48228adac7d2a21b697a99e592d
AmarH <amar.hajrovic@m-trak.com>
2025-11-08 03:23:17 +0100

    [Fix] Profile deletion should force delete not soft delete

--- app/Livewire/Settings/DeleteUserForm.php
+++ app/Livewire/Settings/DeleteUserForm.php
@@ -19,7 +19,7 @@ class DeleteUserForm extends Component
             'password' => ['required', 'string', 'current_password'],
         ]);
 
-        tap(Auth::user(), $logout(...))->delete();
+        tap(Auth::user(), $logout(...))->forceDelete();
 
         $this->redirect('/', navigate: true);
     }
