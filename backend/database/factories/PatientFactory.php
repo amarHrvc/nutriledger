fee3d585cbdaa559a749960a06d5d2b66620643a
AmarH <amar.hajrovic@m-trak.com>
2026-04-03 00:10:24 +0200

    Add hasSocioeconomic() factory state to PatientFactory

--- backend/database/factories/PatientFactory.php
+++ backend/database/factories/PatientFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Patient;
+use App\Models\PatientSocioeconomic;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -39,5 +40,9 @@ class PatientFactory extends Factory
             'medical_notes' => fake()->paragraph(),
             //
         ];
+    }
+
+    public function hasSocioeconomic(): static {
+        return $this->has(PatientSocioeconomic::factory(), 'socioeconomic');
     }
 }
