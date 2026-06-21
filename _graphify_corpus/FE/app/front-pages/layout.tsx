fcd1059f356988f97fc6275fa89035904b0c91ae
Amar Hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2026-06-21 10:55:59 +0200

    [UI]: fix title

--- _graphify_corpus/FE/app/front-pages/layout.tsx
+++ _graphify_corpus/FE/app/front-pages/layout.tsx
@@ -27,9 +27,9 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Vuexy - MUI Next.js Admin Dashboard Template',
+  title: 'NutriBase',
   description:
-    'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
+    'NutriBase'
 }
 
 const Layout = async ({ children }: ChildrenType) => {
